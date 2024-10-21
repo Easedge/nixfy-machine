ae8789c12d37bf23fe9cd10f5b6633f246979b37
Easedge <zhangrundide@gmail.com>
2024-10-21 23:26:09 +0800

    fix change vmware

--- flake.nix
+++ flake.nix
@@ -78,7 +78,7 @@
       # Standalone home-manager configuration entrypoint
       # Available through 'home-manager --flake .#your-username@your-hostname'
       homeConfigurations = {
-        "junglefish@hyperv" = home-manager.lib.homeManagerConfiguration {
+        "junglefish@vmware" = home-manager.lib.homeManagerConfiguration {
           pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
           extraSpecialArgs = { inherit inputs outputs; };
           modules = [
