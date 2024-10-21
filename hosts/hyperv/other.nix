d3f627e2e51feeff42f7489deb1b7cea05e31c16
Easedge <zhangrundide@gmail.com>
2024-10-21 17:16:36 +0800

    delete i18n gliclocale

--- hosts/hyperv/other.nix
+++ hosts/hyperv/other.nix
@@ -39,7 +39,6 @@
 
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.supportedLocales = [ "en_US.UTF-8/UTF-8" "zh_CN.UTF-8/UTF-8" ];
-  i18n.glibcLocales
   i18n.inputMethod = {
     enable = true;
     type = "ibus";
