cf92c12aca6bf234d1d31dd08b31bc0a9389d12d
Easedge <zhangrundide@gmail.com>
2024-10-21 17:10:44 +0800

    fix bugs

--- hosts/hyperv/other.nix
+++ hosts/hyperv/other.nix
@@ -38,15 +38,16 @@
   };
 
   i18n.defaultLocale = "en_US.UTF-8";
-  i18n.supportedLocales = [ "C.UTF-8" "zh_CN.UTF-8" ];
+  i18n.supportedLocales = [ "en_US.UTF-8/UTF-8" "zh_CN.UTF-8/UTF-8" ];
+  i18n.glibcLocales
   i18n.inputMethod = {
     enable = true;
-    enabled = "ibus";
+    type = "ibus";
     ibus.engines = with pkgs.ibus-engines; [
       libpinyin
       rime
     ];
-    # enabled = "fcitx5";
+    # type = "fcitx5";
     # fcitx5.plasma6Support = true;
     # fcitx5.waylandFrontend = true;
     # fcitx5.addons = with pkgs; [
@@ -137,7 +138,7 @@
   programs.direnv.enable = true;
   programs.bash = {
     enableLsColors = true;
-    completion = true;
+    completion.enable = true;
     blesh.enable = true;
     shellInit = ''
       eval "$(direnv hook bash)"
