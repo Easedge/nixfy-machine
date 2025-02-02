214b4d9c6bf22a2c43795ac8e659d2e2520307ee
Easedge <zhangrundide@gmail.com>
2025-02-03 00:32:49 +0800

    vmware change gdm to sddm test

--- hosts/omen/desktop/plasmapackages.nix
+++ hosts/omen/desktop/plasmapackages.nix
@@ -7,8 +7,8 @@
 
 {
   services.desktopManager.plasma6.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
 
   environment.systemPackages =
     with pkgs;
--- hosts/vmware/xserver.nix
+++ hosts/vmware/xserver.nix
@@ -3,7 +3,9 @@
   services.xserver.enable = true;
   services.xserver.excludePackages = [ pkgs.xterm ];
   # services.xserver.displayManager.startx.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.xkb.layout = "cn";
 
