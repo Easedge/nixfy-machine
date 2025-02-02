a395e85be24cff3402ef91050415d43c03a5322f
Easedge <zhangrundide@gmail.com>
2025-02-03 00:55:42 +0800

    add gdm wayland support

--- hosts/omen/desktop/gnomepackages.nix
+++ hosts/omen/desktop/gnomepackages.nix
@@ -6,7 +6,7 @@
 }:
 {
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.wayland =true;
+  services.xserver.displayManager.gdm.wayland = true;
   services.xserver.desktopManager.gnome.enable = true;
 
   services.gnome.gnome-remote-desktop.enable = true;
--- hosts/vmware/xserver.nix
+++ hosts/vmware/xserver.nix
@@ -6,6 +6,7 @@
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.displayManager.gdm.wayland = true;
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.xkb.layout = "cn";
 
